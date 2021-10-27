fde646b37b24654f398c21b0d68bd67e0b6b5491
Anton Lovrić <antonlovri81@gmail.com>
2021-10-27 18:55:28 +0200

    Design is now responsive for the needed viewports

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector(".navigation").classList.toggle("showNavigation");
+  document.querySelector(".pointer").classList.toggle("showPointer");
+});
